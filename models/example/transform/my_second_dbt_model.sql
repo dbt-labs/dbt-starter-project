8b66720e0eed847db76eee8e44f04c9c1179ab27
Erin Ogilvy <erin.ogilvy@gmail.com>
2018-07-06 14:33:48 -0400

    adding to first model structure

---
+++ models/example/transform/my_second_dbt_model.sql
@@ -0,0 +1,24 @@
+--because no configuration is defined in this SQL file, it defaults to "view" as 
+--defined in the dbt_project.yml file
+
+with raw_data as (
+    
+--now that we have the raw data defined in the base model, we can use the most 
+--important dbt function, ref() to reference that model in subsequent queries
+--https://dbt.readme.io/reference#ref    
+    
+    select * from {{ref('my_first_dbt_model')}}
+    
+),
+
+calculations as (
+    
+    select
+        id,
+        id + 10 as add_id,
+        id * 2 as multiply_id
+    from raw_data
+    
+)
+
+select * from calculations
